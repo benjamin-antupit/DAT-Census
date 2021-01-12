f641589b1ceb3f9e71ec9c192d8bbd3751d66b64
salmogy22 <77023959+salmogy22@users.noreply.github.com>
2021-01-11 21:03:52 -0800

    Add files via upload

---
+++ weighted-sample.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+
+def weighted_sample(df, column, weights, n):
+    indexWeights = []
+    counts = df[column].value_counts()
+    for key in weights:
+        weights[key] = weights[key]/counts[key]
+    for row in df.index:
+        indexWeights.append(weights[df[column][row]])
+    return df.sample(n, weights=indexWeights)
