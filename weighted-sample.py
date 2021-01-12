992ec4fc69ae397c55a2bda40579088d3c07ff2d
salmogy22 <77023959+salmogy22@users.noreply.github.com>
2021-01-12 11:24:16 -0800

    Update weighted-sample.py

--- weighted-sample.py
+++ weighted-sample.py
@@ -1,7 +1,13 @@
 import pandas as pd
 import numpy as np
 
-def weighted_sample(df, column, weights, n):
+def weighted_sample(df, column, n, weights=None):
+    if not weights:
+        weights = {}
+        values = df[column].unique()
+        for value in values:
+            weights[value] = 1/len(values)
+        
     indexWeights = []
     counts = df[column].value_counts()
     
