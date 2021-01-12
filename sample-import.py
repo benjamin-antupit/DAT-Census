4c7e97e13ce62e76f0b18f22a410d1c2967f34e8
Benjamin A <yellowjaguar5@gmail.com>
2021-01-12 11:44:32 -0800

    Add secondary data

--- sample-import.py
+++ sample-import.py
@@ -3,10 +3,9 @@ import numpy as np
 
 
 def sample_import(file_name):
-    data = pd.read_csv(file_name)
-    data[0].unique()
+    data = pd.read_csv(file_name, header=[0, 1])
     print(data.head())
     print(data.describe())
 
 
-sample_import("input/Census Data Generation Mock Survey_January 11, 2021_18.36 - Sheet1.csv")
+sample_import("input/Census Data Generation Mock Survey_January 11, 2021_18.36 - Sheet1 (1).csv")
