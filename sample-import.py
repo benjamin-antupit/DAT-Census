5fbba6cd9c43f4be4be86ee383840414a3f3964b
Benjamin A <yellowjaguar5@gmail.com>
2021-01-12 11:24:08 -0800

    Update sample-import.py

--- sample-import.py
+++ sample-import.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import numpy as np
 
+
 def sample_import(file_name):
     data = pd.read_csv(file_name)
+    data[0].unique()
     print(data.head())
     print(data.describe())
+
 
 sample_import("input/Census Data Generation Mock Survey_January 11, 2021_18.36 - Sheet1.csv")
