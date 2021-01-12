f263c6a6f0ef0dcf4934d9f2caa0ed8c7c5ec794
Kyler <kylerrosen@gmail.com>
2021-01-12 11:13:58 -0800

    spacing

--- weighted-sample.py
+++ weighted-sample.py
@@ -4,8 +4,11 @@ import numpy as np
 def weighted_sample(df, column, weights, n):
     indexWeights = []
     counts = df[column].value_counts()
+    
     for key in weights:
         weights[key] = weights[key]/counts[key]
+
     for row in df.index:
         indexWeights.append(weights[df[column][row]])
+
     return df.sample(n, weights=indexWeights)
