a65e9e541493484d8c69cb42b07ce23d58406218
Benjamin A <yellowjaguar5@gmail.com>
2021-01-14 11:18:08 -0800

    tabs --> spaces

--- stratified-sample.py
+++ stratified-sample.py
@@ -2,21 +2,22 @@ import pandas as pd
 import numpy as np
 import random_sample
 
+
 def stratified_sample(df, column, n, weights=None):
-	if not weights:
+    if not weights:
         weights = {}
         values = df[column].unique()
-        
-        for value in values:
-            weights[value] = 1/len(values)
 
-    l = [0]*len(df.columns)
+        for value in values:
+            weights[value] = 1 / len(values)
+
+    l = [0] * len(df.columns)
     i = 0
     rows = len(df.index)
 
-	for region, df_region in df.groupby(column):
-		l[i] = df_region
-		i+=1
+    for region, df_region in df.groupby(column):
+        l[i] = df_region
+        i += 1
 
-	slices = [pfSlice.random_sample(len(pfSlice.index)//size) for pfSlice in l]
-	df_final = pd.concat(slices)
+    slices = [pfSlice.random_sample(len(pfSlice.index) // size) for pfSlice in l]
+    df_final = pd.concat(slices)
