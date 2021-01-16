511d6a7575e4a7b309637a8b31c2ae64b318b8e2
salmogy22 <77023959+salmogy22@users.noreply.github.com>
2021-01-16 14:37:38 -0800

    Update weighted-sample.py

--- weighted-sample.py
+++ weighted-sample.py
@@ -3,17 +3,19 @@ import numpy as np
 
 def weighted_sample(df, column, n, weights=None):
     if not weights:
+        #weight each distinct value in column equally
         weights = {}
-        values = df[column].unique()
+        values = df[column].unique() #list of all distinct values in the column
         for value in values:
             weights[value] = 1/len(values)
         
-    indexWeights = []
-    counts = df[column].value_counts()
+    indexWeights = [] #weights of individual responses
+    counts = df[column].value_counts() #series with number of occurences of each value
     
     for key in weights:
-        weights[key] = weights[key]/counts[key]
+        weights[key] = weights[key]/counts[key] #total weight of all rows with value key will be the original weight[key]
 
+    #each row is assigned the appropriate weight
     for row in df.index:
         indexWeights.append(weights[df[column][row]])
 
