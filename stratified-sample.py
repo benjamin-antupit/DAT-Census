a0c0e77f11149703414072da5b957d86c5f735c6
Kyler <kylerrosen@gmail.com>
2021-01-17 01:48:56 -0800

    Commented

--- stratified-sample.py
+++ stratified-sample.py
@@ -1,23 +1,30 @@
 import pandas as pd
 import numpy as np
 import random_sample
+import sample_import
 
 
-def stratified_sample(df, column, n, weights=None):
-    if not weights:
-        weights = {}
-        values = df[column].unique()
+def stratified_sample(df, column):
+    #Data
+    gender = {"Female":243/472, "Male":212/472, "Other":17/472}
+    race = {"American Indian or Alaska Native":4/472,"Asian":231/472}
 
-        for value in values:
-            weights[value] = 1 / len(values)
-
-    l = [0] * len(df.columns)
+    #empty list to store dfSlices
+    dfSlices = [0] * len(df.columns)
     i = 0
-    rows = len(df.index)
 
+    #breaks up slices by column parameter, stores them in empty list
     for region, df_region in df.groupby(column):
-        l[i] = df_region
+        dfSlices[i] = df_region
         i += 1
 
-    slices = [pfSlice.random_sample(len(pfSlice.index) // size) for pfSlice in l]
-    df_final = pd.concat(slices)
+    #determines where to search for the percentages for each bucket
+    if lower(column) == "gender": size = gender
+    elif lower(column) == "Race": size = race
+
+    #creates list of the buckets
+    slices = [dfSlice.random_sample(len(dfSlice.index) * size[dfSlice[column].iloc[0]]) for dfSlice in dfSlices]
+
+    #sticks them together, returns
+    return pd.concat(slices)
+
