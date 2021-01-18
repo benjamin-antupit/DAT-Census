3b8aa871eae35006534b68bca892a7353f2f4189
Benjamin A <yellowjaguar5@gmail.com>
2021-01-18 10:01:26 -0800

    add mult choice and free response options

--- parser.py
+++ parser.py
@@ -33,11 +33,15 @@ def parse(file_name: str) -> ():
 
 
 def getMultipleChoice(df: pd.DataFrame):
-    return df.drop(columns=[])
+    return df.drop(columns=["Q1_5_TEXT", "Q16", "Q18", "Q22_4_TEXT", "Q26_11_TEXT"])
 
 
 def getFreeTextResponse(df: pd.DataFrame):
-    return df.drop(columns=[])
+    return df.drop(columns=["Q1_1", "Q1_2", "Q1_3", "Q1_4", "Q1_5", "Q3", "Q4_1", "Q5", "Q6_1", "Q6_2", "Q6_3", "Q6_4",
+                            "Q9_1", "Q9_2", "Q9_3", "Q9_4", "Q10_1", "Q12_1", "Q12_2", "Q12_3", "Q12_4", "Q12_5",
+                            "Q12_6", "Q12_7", "Q13_1", "Q13_2", "Q14_1", "Q14_2", "Q14_3", "Q14_4", "Q15_1", "Q15_2",
+                            "Q15_3", "Q15_4", "Q17", "Q19_1", "Q19_2", "Q19_3", "Q19_4", "Q19_5", "Q19_6", "Q19_7",
+                            "Q21", "Q22", "Q23", "Q24", "Q25", "Q26", "Q27"])
 
 
 def main():
@@ -59,31 +63,31 @@ def main():
 
     if "1" in options or "0" in options:
         # parsed only
-        outputs.append(("Parsed", data.copy()))
+        outputs.append(("1_Parsed", data.copy()))
 
     if "2" in options or "0" in options:
         # TODO mult choice only
-        outputs.append(("Parsed_Multiple_Choice", getMultipleChoice(data.copy())))
+        outputs.append(("2_Parsed_Multiple_Choice", getMultipleChoice(data.copy())))
 
     if "3" in options or "0" in options:
         # TODO free response only
-        outputs.append(("Parsed_Free_Response", getFreeTextResponse(data.copy())))
+        outputs.append(("3_Parsed_Free_Response", getFreeTextResponse(data.copy())))
 
     if "4" in options or "0" in options:
         # simple random
-        outputs.append(("Simple_Random_Sample_Parsed", random_sample(data.copy(), None, 0.5)))
+        outputs.append(("4_Simple_Random_Sample_Parsed", random_sample(data.copy(), None, 0.5)))
 
     if "5" in options or "0" in options:
         # stratified by grade
-        outputs.append(("Stratified_Race_Parsed", stratified_sample(data.copy(), "Q24", fraction=0.5)))
+        outputs.append(("5_Stratified_Race_Parsed", stratified_sample(data.copy(), "Q24", fraction=0.25)))
 
     if "6" in options or "0" in options:
         # stratified by how many people you live with
-        outputs.append(("Stratified_Gender_Parsed", stratified_sample(data.copy(), "Q22", sample_size=4)))
+        outputs.append(("6_Stratified_Gender_Parsed", stratified_sample(data.copy(), "Q22", fraction=0.5)))
 
     if "7" in options or "0" in options:
         # weighted by parent education
-        outputs.append(("Weighted_Education_Parsed", weighted_sample(data.copy(), "Q25", None)))
+        outputs.append(("7_Weighted_Education_Parsed", weighted_sample(data.copy(), "Q25", frac=0.5)))
 
     createAllOutputs(outputs, header_labels)
 
