235b8e997a7eb926451f26858e2c469eea09e77c
Kyler <kylerrosen@gmail.com>
2021-01-18 09:35:35 -0800

    mixed fix

--- stratified_sample.py
+++ stratified_sample.py
@@ -4,9 +4,13 @@ from random_sample import random_sample
 
 
 def stratified_sample(df, column, n):
+    #temporary solution until more data on mixed is recieved
+    print(df['Q24'])
+    if column == "Q24":
+        df.loc[df['Q24'].str.contains(','), 'Q24'] = 'Multiracial'
 
-    #Remove, temporary solution to the issue of dealing with mixed differnt than the DEI survey
-    df.drop(df.tail(1).index,inplace=True)
+    #Remove, temporary solution to remove the 1 mixed response until they change their survey to just say mixed
+    #df.drop(df.tail(1).index,inplace=True)
     #Data
     #At some point, this would be great as an import, however at this time the file is not computer readable so hardcode is faster
     #Non-Binary -- No Data
@@ -15,6 +19,7 @@ def stratified_sample(df, column, n):
     #Remove East Asian tester
     #Indian -- No data
     race = {"American Indian or Alaskan Native":4/472,"Indian":0/472,"East Asian":231/472,"Other Asian":231/472,"Black or African American":20/472,"Hispanic or Latino":45/472,"Middle Eastern":15/472,"Multiracial":61/472,"Native Hawaiian or Other Pacific Islander":15/472,"White":203/472,"Other":16/472,"I prefer not to respond":19/472}
+    #Missing all data
     grade = {6:0, 7:0, 8:0, 9:0, 10:0, 11:0, 12:0}
 
     #empty list to store dfSlices
@@ -22,14 +27,13 @@ def stratified_sample(df, column, n):
 
     #breaks up slices by column parameter, stores them in empty list
     for df_region in df.groupby(by=[column]).__iter__():
-        dfSlices.append(df_region)
-
+        if df_region[1][column].iloc[0] != "":
+            dfSlices.append(df_region)
 
     #determines where to search for the percentages for each bucket
     if column == "Q22": size = gender
     elif column == "Q23": size = grade
     elif column == "Q24": size = race
-
 
     #creates list of the buckets
     slices = [random_sample(dfSlice[1], round(n * size[dfSlice[1][column].iloc[0]])) for dfSlice in dfSlices]
